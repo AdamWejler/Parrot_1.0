76c88cad863ebca6dbcc7f293d21c85f869f7537
AdamWejler <adam.wejler@op.pl>
2015-12-10 09:08:48 +0100

    Revert "Dźwięki"
    This reverts commit f5a1bba7c3c4efdcfdb0a375ff25e7b58b1abb19.

--- Parrot_1.0/Parrot_header.h
+++ Parrot_1.0/Parrot_header.h
@@ -2,19 +2,9 @@
 	#include<allegro5\allegro.h>
 	#include<allegro5\allegro_native_dialog.h>
 	#include<allegro5\allegro_image.h>
-	#include<iostream>
-	#include<process.h>							//Do obslugi watkow
-	#include<time.h>							//Do obslugi czasu
-	#include<stdlib.h>
-	#include<allegro5\allegro_audio.h>
-	#include<allegro5\allegro_acodec.h>
+	#include<string>
 
-	
-
-
-	using namespace std;
-
-//Deklaracja zmiennych bitmapowych
+//Deklaracja zmiennych
 
 	ALLEGRO_DISPLAY *display;			//Okno programu
 	ALLEGRO_BITMAP *mainMenu;			//Zdjecie dla menu glownego
@@ -25,32 +15,8 @@
 	ALLEGRO_BITMAP *instruction;		//Zdjecie dla Wprowadzenie i Instrukcja
 	ALLEGRO_BITMAP *results;			//Zdjecie dla Wyniki
 	ALLEGRO_BITMAP *piano;				//Zdjecie na glowny tryb gry
-	ALLEGRO_BITMAP *piano_started;		//Zdjecie po uruchomieniu stopera
-	ALLEGRO_BITMAP *piano_Q;
-	ALLEGRO_BITMAP *piano_W;
-	ALLEGRO_BITMAP *piano_E;
-	ALLEGRO_BITMAP *piano_R;
-	ALLEGRO_BITMAP *piano_T;
-	ALLEGRO_BITMAP *piano_Y;
-	ALLEGRO_BITMAP *piano_U;
-	ALLEGRO_BITMAP *piano_I;
-	ALLEGRO_BITMAP *piano_O;
-	ALLEGRO_BITMAP *piano_P;
-
-//Deklaracja zmiennych dzwiekowych
-
-	ALLEGRO_SAMPLE *sound_on_Q;
-	ALLEGRO_SAMPLE *sound_on_W;
-	ALLEGRO_SAMPLE *sound_on_E;
-	ALLEGRO_SAMPLE *sound_on_R;
-	ALLEGRO_SAMPLE *sound_on_T;
-	ALLEGRO_SAMPLE *sound_on_Y;
-	ALLEGRO_SAMPLE *sound_on_U;
-	ALLEGRO_SAMPLE *sound_on_I;
-	ALLEGRO_SAMPLE *sound_on_O;
-	ALLEGRO_SAMPLE *sound_on_P;
-
-	ALLEGRO_EVENT_QUEUE *event_queue;				//Zdarzenie
+	ALLEGRO_TIMER *timer;				//Timer
+	ALLEGRO_EVENT_QUEUE *event_queue;	//Zdarzenie
 
 	enum mainMenuChoices {ROZPOCZNIJ_GRE, WYNIKI, WPROWADZENIE_I_INSTRUKCJA, WYJDZ_Z_GRY};
 	enum activeVindows {MENU_GLOWNE, GRA, INFORMACJA_O_WYNIKU};
@@ -60,20 +26,6 @@
 	int mainMenuChoice = NULL;					//Wybor z menu glownego
 	int activeWindow = MENU_GLOWNE;				//Aktualne okno
 	int mainMenuButtonCoursored = NULL;			//Przycisk nakierowany kursorem
-	int stopper = 0;							//Aktualny stan stopera
-
+	
 	bool leftMouseButtonClicked = false;
-	bool startClicked = false;
 
-
-//Funkcje watkowe
-	void start_stopper(void* dummy)
-	{
-		while(true)
-		{
-			Sleep(1000);		//Czeka 1 sekunde = 1000 milisekund
-			stopper++;
-			cout<<stopper<<endl;
-		}
-		_endthread();		//Zakoñczenie w¹tku
-	}
